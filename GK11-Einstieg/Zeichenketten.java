6694b733cfede204064bf06e2b2e1b42259cc418
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2020-02-13 14:06:36 +0100

    Stunde vom 12.02.

--- GK11-Einstieg/Zeichenketten.java
+++ GK11-Einstieg/Zeichenketten.java
@@ -76,7 +76,7 @@ public class Zeichenketten {
         test("ollaH", reverse2("Hallo"));
         test("ZYXWVUTSRQPONMLKJIHGFEDCBA", reverse2("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
         test("einnegermitgazellezagtimregennie", reverse2("einnegermitgazellezagtimregennie"));
-        
+
         test("Junu", ersetzeA("Jana"));
         test("Ubruhum", ersetzeA("Abraham"));
     }
@@ -88,6 +88,42 @@ public class Zeichenketten {
             System.out.println("\nFEHLER! " + 
                 erwartet + " != " + ergebnis );
         }
+    }
+
+    String text = "Drei Chinesen mit dem Kontrabass\n"+
+        "saßen auf der Straße und erzählten sich was.\n"+
+        "Da kam die Polizei, ei was ist denn das?\n"+
+        "Drei Chinesen mit dem Kontrabass.";
+
+    public boolean istVokal(char buchstabe) {
+        String vokale = "aeiouAEIOUäöüÄÖÜ";
+        
+    }
+
+    public String chinesen1(String input) {
+        // 1. Definiere output
+        String output = "";
+
+        // 2. Bastel die Rückgabe Zeichen für Zeichen zusammen
+        for (int i = 0; i < input.length(); i = i + 1) {
+            char c = input.charAt(i);
+
+            // ist c ein "e"?
+            // TODO: Große Vokale
+            // TODO: >1 Vokale hintereinander
+            if (c == 'a' || c == 'e' || c == 'i' || c=='o' || c=='u' ) {
+                output = output + 'ä';
+            } else {
+                output = output + c;
+            }
+        }
+
+        // 3. Gib das Ergebnis als Text zurück
+        return output;
+    }
+
+    public void testChinesen1() {
+        System.out.println( chinesen1(text) );
     }
 }
 
